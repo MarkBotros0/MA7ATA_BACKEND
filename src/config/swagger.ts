c96182714422ccdc7f556b33ec3d3e0f33ae7e8a
backend <markbotro0@gmail.com>
2025-03-18 23:07:28 +0200

    fix swagger title

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -3,7 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export function setupSwagger(app: INestApplication): void {
   const config = new DocumentBuilder()
-    .setTitle('Qoodz')
+    .setTitle('Ma7ata')
     .setVersion('1.0')
     .addApiKey(
       {
